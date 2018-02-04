6c024cd70a78103b223a2a1341590f3d43449fa1
Kishor Gujar <kishorgujar95@gmail.com>
2018-02-04 15:53:27 +0530

    Profile added to serve

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../../services/profile.service';
 import { Profile } from '../../classes/profile';
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor(private profileService: ProfileService) { }
+  options: FormGroup;
+  
+  constructor(private profileService: ProfileService, fb: FormBuilder) {
+    this.options = fb.group({
+      hideRequired: false,
+      floatLabel: 'auto',
+    });
+  }
 
   ngOnInit() {
   
